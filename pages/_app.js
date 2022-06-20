fb3610584dcecae0b7fbc3ce827402f5b6fa6feb
myk <m2kio@pm.me>
2022-06-20 15:48:57 +0300

    tailwindcss config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Tailwindcss!</h1>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
